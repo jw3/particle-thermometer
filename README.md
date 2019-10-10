caaaa8eaf41bd71a8812b7668685a7ed3c357136
John Wass <jwass3@gmail.com>
2019-10-09 22:07:04 -0400

    basic functionality

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 particle thermometer
 ===
 
+
+### dev
 - `conan install . -if cmake-build-debug -s compiler.version=5`
 - `./build.sh photon`
 - `./build-photon/flash firmware usb`
+
+### config
+- `docker run --rm -it --privileged -v /dev:/dev ctcoss/particle-cli identify`
+- `docker run --rm -it --privileged -v /dev:/dev ctcoss/particle-cli serial monitor`
+- `docker run --rm -it --privileged -v /dev:/dev --entrypoint="" ctcoss/particle-cli bash`
--- firmware/application.cpp
+++ firmware/application.cpp
@@ -1,5 +1,24 @@
+#include <Particle.h>
+#include <OneWire.h>
+#include <spark-dallas-temperature.h>
+
+long previous = 0;
+long interval = 5 * 1000;
+
+OneWire w1(D4);
+DallasTemperature sensor(&w1);
+SerialLogHandler logHandler;
+
 void setup() {
+   sensor.begin();
 }
 
 void loop() {
+   if(millis() - previous > interval) {
+      sensor.requestTemperatures();
+      String s(sensor.getTempFByIndex(0));
+      previous = millis();
+      Particle.publish("T", s);
+      Log.info("sensed %s ℉", s.c_str());
+   }
 }
